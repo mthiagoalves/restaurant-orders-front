ddc31a20b53fbe26fde6614ed300d8cef4076140
mthiagoalves <m.thiago.alves@live.com>
2023-08-31 18:30:37 +0100

    create(style): create assets to projects

---
+++ src/assets/styles/constants.ts
@@ -0,0 +1,13 @@
+export const constants = {
+    logoFontFamily: 'Big Shoulders Inline Display',
+    logoFontSize: '1.75rem',
+    logoLineHeight: '140%',
+
+    headingFontFamily: 'Barlow',
+    headingFontSize: '1.75rem',
+    headingLineHeight: '140%',
+
+    bodyFontFamily: 'Barlow',
+    bodyFontSize: '0.875',
+    bodyLineHeight: '140%',
+}
